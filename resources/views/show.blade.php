1f8020ff08bd499a3127aed7bde663cd3a53fcea
PBoskovic <milanoslav@yahoo.com>
2018-02-05 21:37:01 +0100

    deleting duplicate vaules and adding suggestions

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,56 +13,57 @@
             {{--FRIENDS OF USER--}}
             <div class="col-lg-12 custom"><h3>List of {{$user->firstName}}'s friends</h3></div>
             @foreach($user->friends as $friend)
-                @include('layouts.friends')
+                @include('layouts.friends.friends')
             @endforeach
 
             {{--FRIENDS OF FRIENDS--}}
-            {{--<ul class="list-group list-group-flush">--}}
-            {{--<li class="list-group-item">Name: {{ $user->firstName }} {{ $user->surname }}</li>--}}
-            {{--<li class="list-group-item">Friends:--}}
-            {{--@foreach($user->friends as $friend)--}}
-            {{--{{ $friend->firstName . ', ' }}--}}
-            {{--@endforeach--}}
-            {{--</li>--}}
+
             <div class="col-lg-12 custom">
                 <h3> List of friends of {{$user->firstName}}'s friend</h3>
             </div>
 
-            <?php  $listf = []; ?>
+            <?php  $listf = [];
+            $listFof = [];?>
             @foreach($user->friends as $friend)
                 <?php  $listf[] = $friend->id; ?>
             @endforeach
+
+            @foreach($user->friends as $friend)
+                @foreach($friend->friends as $fof)
+                    <?php $listFof[] = $fof->id; ?>
+                @endforeach
+            @endforeach
+
+            <?php ?>
             @foreach($user->friends as $friend)
                 @foreach($friend->friends as $fof)
                     @if($fof->id != $user->id && $fof->id!= $friend->id && !in_array($fof->id,$listf))
-                        @include('layouts.friendsOfFriends')
+                        @include('layouts.friends.friendsOfFriends')
                     @endif
                 @endforeach
             @endforeach
 
+            {{--SUGGESTED FRIENDS--}}
+            <div class="col-lg-12 custom">
+                <h3> Suggested friends for {{$user->firstName}}</h3>
+            </div>
 
+            <?php function array_count_values_of($value, $array)
+            {
+                $counts = array_count_values($array);
+                return $counts[$value];
+            }
+//            dd($listFofU=array_unique($listFof));
+            ?>
+            @foreach($user->friends as $friend)
+                @foreach($friend->friends as $fof)
+                    @if($fof->id != $user->id && $fof->id!= $friend->id && !in_array($fof->id,$listf) && array_count_values_of($fof->id,$listFof)>1)
+                        @include('layouts.friends.friendsOfFriends')
 
-            {{--<ul>--}}
+                    @endif
+                @endforeach
+            @endforeach
 
-            {{--@foreach($user->friends as $friend)--}}
-            {{--{{ $friend->firstName . ', ' }}--}}
-            {{--@foreach($friend->friends as $ff)--}}
-            {{--@if( $ff->id != $user->id)--}}
-            {{--{{ $ff->firstName }}--}}
-            {{--@endif--}}
-            {{--@endforeach--}}
-            {{--@endforeach--}}
-            {{--</li>--}}
-            {{--<li>Prijatelji od prijatelja:--}}
-            {{--@foreach($user->friends as $friend)--}}
-            {{--@foreach($friend->friends as $ff)--}}
-            {{--@if($ff->id != $user->id)--}}
-            {{--{{ $ff->firstName . ', ' }}--}}
-            {{--@endif--}}
-            {{--@endforeach--}}
-            {{--@endforeach--}}
-            {{--</li>--}}
-            {{--</ul>--}}
 
         </div>
     </div>
